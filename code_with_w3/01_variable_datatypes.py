1132e741e3f770c7bc38c050c411c279fe5d4ca8
biplob-kb <biplobkb123@gmail.com>
2025-01-30 11:59:54 +0600

    updated the variable

--- code_with_w3/01_variable_datatypes.py
+++ code_with_w3/01_variable_datatypes.py
@@ -1,2 +1,28 @@
 
-print("Hello Python")
+# --------------------------Initialize variable-------------------->
+
+# productName = input("Enter product name : ")
+# productPrice = input("Enter product price : ")
+# quantity = input("Enter product quantity : ")
+
+# print("Product name : ", productName)
+# print("Product price : ", productPrice)
+# print("Product Quantity : ", quantity)
+
+
+
+#------------------------- data types-------------------------------->
+# Text Type  : str
+# Numeric Type : int, float, complex
+# Sequence Type : list, tuple, range
+# Mapping Type : dict
+# Set Type : set, frozenset
+# Boolean Type : bool
+# Binary Type : bytes, bytearray, memoryview
+# None Type : NoneType
+
+x = 5
+print(type(x))
+
+
+
