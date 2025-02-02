19b8b7bb4c97a285f34ff5a3415dbbe7c75fe66e
biplob-kb <biplobkb123@gmail.com>
2025-02-02 20:11:17 +0600

    completed the string

---
+++ code_with_w3/02_string.py
@@ -0,0 +1,52 @@
+#-----------------------Declaration----------------------->
+# print("Hello Python")
+
+# a = """ I am Biplob,
+# Now I am ready with python
+# """
+
+# print(a)
+
+
+#----------------------Slicing string---------------------->
+# a = input("Enter a word or sentence : ")
+
+# print(a)
+# print(a[0])
+# print(a[:4])
+# print(a[1:5])
+# print(a[2:])
+# print(a[-5:-2])
+
+
+#------------------------Modify string-----------------------
+# a = input("Enter a word or sentence : ")
+
+# print(a.upper())
+# print(a.lower())
+# print(a.split(","))
+# print(a.replace("H", "J"))
+# print(a.strip())
+
+
+
+#-------------------Concatenate string-------------------------
+# fName = input("Enter your first name : ")
+# lName = input("Enter your last name : ")
+
+# fullName = fName + " "+ lName
+
+# print(fullName)
+
+
+
+
+#------------------------------Format String-----------------------
+
+age = 30
+txt = f"My age is {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
