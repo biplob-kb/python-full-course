6f300a34561bd9d7ba90ed2f90faaa0f4648b2d3
biplob-kb <biplobkb123@gmail.com>
2025-02-02 19:49:31 +0600

    completed the data types

--- code_with_w3/01_variable_datatypes.py
+++ code_with_w3/01_variable_datatypes.py
@@ -21,8 +21,51 @@
 # Binary Type : bytes, bytearray, memoryview
 # None Type : NoneType
 
-x = 5
-print(type(x))
+# x = 5
+# print(type(x))
+
+# y = "Hello Python"
+# print(type(y))
+
+
+#------------------------------Number types---------------------------------
+
+# int
+# x = 10
+# y = 2929929
+# z = -989898
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# float
+# a = 5.50
+# b = - 10.25
+# m = 3e3
+# n = 10e4
+# print(type(a))
+# print(type(b))
+# print(type(m))
+# print(type(n))
+
+
+# complex
+# k = 3 + 4j
+# l = -5j
+# print(type(k))
+# print(type(l))
 
 
 
+
+# -------------------------------- type casting------------------------
+f = 10.25
+print(int(f))
+
+i = 10
+print(float(i))
+
+z = "3"
+print(int(z))
