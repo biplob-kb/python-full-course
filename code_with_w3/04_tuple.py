0f79b11ef907576e16f913d51d459804cf9992c7
biplob-kb <biplobkb123@gmail.com>
2025-02-03 18:38:24 +0600

    completed the tuple

--- code_with_w3/04_tuple.py
+++ code_with_w3/04_tuple.py
@@ -8,7 +8,7 @@
 # Dictionary => Dictionary is a collection which is (ordered), (changeable) and allows no duplicate members. {"brand": "Sony", "model":"Xperia 5"}
 
 
-#---------------------Declaration-----------------------------------
+#---------------------------Declaration-----------------------------------
 # myTuple = ("apple", "orang", "banana")
 # myTuple2 = ("apple", 2, 10.25, True, "apple")
 # print(myTuple)
@@ -16,5 +16,46 @@
 # print(len(myTuple))
 
 # print(myTuple2)
+
+
+
+#----------------------Access Tuple------------------------------------
+# myTuple = ("apple", "orange", "banana", "lichi", "mango")
+
+# print(myTuple[0])
+# print(myTuple[1:4])
+
+# for c in myTuple:
+#     print(c)
+
+
+# for i in range(len(myTuple)):
+#     print(myTuple[i])
+
+
+
+# if "apple" in myTuple:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+#------------------------ Update Tuples--------------------------
+myTuple = ("apple", "orange", "banana", "lichi", "mango")
+
+# myTuple[0] = "pineapple"
+# print(myTuple)
+
+list1 = list(myTuple)
+list1[0] = "pineapple"
+myTuple = tuple(list1)
+print(myTuple)
+
+
+# to update or add  tuple information firstly we need to convert the tuple into list then after changing again we need to convert it into tuple
+
+
+
 
 
