7e0fd0853035fd9a6037400a80b20813804f6bde
biplob-kb <biplobkb123@gmail.com>
2025-02-04 18:45:54 +0600

    practicing the loop with printing star

--- code_with_w3/08_loops.py
+++ code_with_w3/08_loops.py
@@ -1,3 +1,20 @@
 
-for i in range(5):
-    print(i)
+# for i in range(5):
+#     print(i)
+
+row = input("Enter the row : ");
+
+
+for i in range(int(row)):
+    for j in range(int(row)):
+        if j <=i:
+            print("*", end=" ")
+    print()
+
+for i in range(int(row), -1, -1):
+    for j in range(int(row)):
+        if j<i:
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    print()
